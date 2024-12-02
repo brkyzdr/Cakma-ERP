915f0e20edc07c591fee2f407592355f05f9d28f
Berkay Ozder <berkayq3535@gmail.com>
2024-12-02 16:20:12 +0300

    CRUD class created

---
+++ CakmaERP/CakmaERP/CRUD.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CakmaERP
+{
+    /// <summary>
+    /// Create-Read-Update-Delete operations for database.
+    /// </summary>
+    public class CRUD
+    {
+        private static readonly string connectionString = "Data Source=...;Initial Catalog=...;Integrated Security=True";
+
+        public static void Create(string tableName, Dictionary<string, object> data)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                string columns = string.Join(", ", data.Keys);
+                string parameters = string.Join(", ", data.Keys.Select(k => "@" + k));
+                string query = $"INSERT INTO {tableName} ({columns}) VALUES ({parameters})";
+
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    foreach (var item in data)
+                    {
+                        command.Parameters.AddWithValue("@" + item.Key, item.Value);
+                    }
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public static DataTable Read(string query)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlDataAdapter adapter = new SqlDataAdapter(query, connection);
+                DataTable dataTable = new DataTable();
+                adapter.Fill(dataTable);
+                return dataTable;
+            }
+        }
+
+        public static void Update(string tableName, Dictionary<string, object> data, string condition)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                string setClause = string.Join(", ", data.Keys.Select(k => $"{k} = @{k}"));
+                string query = $"UPDATE {tableName} SET {setClause} WHERE {condition}";
+
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    foreach (var item in data)
+                    {
+                        command.Parameters.AddWithValue("@" + item.Key, item.Value);
+                    }
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public static void Delete(string tableName, string condition)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                string query = $"DELETE FROM {tableName} WHERE {condition}";
+
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+}
---
+++ CakmaERP/CakmaERP/MainForm.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CakmaERP;
+
+namespace CakmaERP
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        //CRUD class usage example
+        private void Create()
+        {
+            Dictionary<string, object> newData = new Dictionary<string, object>
+            {
+                { "Name", "Ali" },
+                { "Age", 25 },
+                { "Email", "ali@example.com" }
+            };
+            CRUD.Create("Employees", newData);
+        }
+        private void Read()
+        {
+            DataTable dataTable = CRUD.Read("SELECT * FROM Employees");
+            //ex://dataGridView1.DataSource = dataTable;
+        }
+        private void Update()
+        {
+            Dictionary<string, object> updatedData = new Dictionary<string, object>
+            {
+                { "Name", "Veli" },
+                { "Age", 30 }
+            };
+            CRUD.Update("Employees", updatedData, "Id = 1");
+        }
+        private void Delete()
+        {
+            CRUD.Delete("Employees", "Id = 1");
+        }
+
+    }
+}
